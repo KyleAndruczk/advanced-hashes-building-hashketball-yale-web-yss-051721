7c55c2dbe150ee5ecf7e161a93a8dcf61a42925e
Kyle Andruczk <kyle.andruczk@gmail.com>
2021-05-28 02:59:18 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -27,8 +27,8 @@ def game_hash
       
       :colors=>[],
       
-      :players=>[ { :player_name=> ""}, { :number=> }, { :shoe=> }, { :points=>}, { :rebounds=> }, 
-        { :assists=> }, { :steals=> }, { :blocks=> }, { :slam_dunks=> }
+      :players=>[ { :player_name=> "",  :number=> ,  :shoe=> ,  :points=>,  :rebounds=> , 
+         :assists=> ,  :steals=> ,  :blocks=> ,  :slam_dunks=> }
       ]
       
       
