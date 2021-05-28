faae3d88ba4e553b3b80896b6fe8883f4340a5a2
Kyle Andruczk <kyle.andruczk@gmail.com>
2021-05-28 03:01:47 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -10,8 +10,8 @@ def game_hash
       # colors
       :colors=>["Black", "White"],
       # players (AoH)
-      :players=>[ { :player_name=> "",  :number=> ,  :shoe=> ,  :points=>,  :rebounds=> , 
-         :assists=> ,  :steals=> ,  :blocks=> ,  :slam_dunks=> }
+      :players=>[ { :player_name=>"Alan Anderson",  :number=>0,  :shoe=>16,  :points=>22,  :rebounds=>12, 
+         :assists=>12,  :steals=> 3,  :blocks=>1,  :slam_dunks=>1 }
       ]
       
       
